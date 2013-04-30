31494d4cd4cdb4ec88c066270d7509ec0fb3f993
Maxime Cormier <mailmaximecormier@gmail.com>
2013-04-30 19:41:32 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sketchmine::Application.config.secret_token = '438e194f6f5b16c19ae159fc9cabcd0bdfd668b3c7e8dbac95b152439936d9930e654bae161520195c7db534300a28e67d7f2c5a6f584f4001eb7d3f115def9a'
